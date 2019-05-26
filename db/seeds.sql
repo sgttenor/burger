c3af34cb6482ef9de1fd4ee1d58d921fa987d4f1
Marco Teti <sgttenore.gmail.com>
2019-05-26 09:57:06 -0700

    added starting fiels for burger app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Portabella Mushroom Burge');
+INSERT INTO cats (burger_name) VALUES ('Pork Belly Burger', true);
